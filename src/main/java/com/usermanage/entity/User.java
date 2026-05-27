da6ffb3c8394a0fe392f4f2344df96282229d415
Billhzx <140683504+Billhzx@users.noreply.github.com>
2026-05-27 20:23:09 +0800

    Add src/main/java/com/usermanage/entity/User.java

---
+++ src/main/java/com/usermanage/entity/User.java
@@ -0,0 +1,109 @@
+package com.usermanage.entity;
+
+/**
+ * 用户实体类
+ * 对应数据库 users 表
+ */
+public class User {
+
+    /** 用户ID */
+    private int id;
+
+    /** 用户名 */
+    private String username;
+
+    /** 密码 */
+    private String password;
+
+    /** 邮箱 */
+    private String email;
+
+    /** 手机号 */
+    private String phone;
+
+    /** 角色 */
+    private String role;
+
+    /** 无参构造方法 */
+    public User() {
+    }
+
+    /**
+     * 全参构造方法
+     *
+     * @param id       用户ID
+     * @param username 用户名
+     * @param password 密码
+     * @param email    邮箱
+     * @param phone    手机号
+     * @param role     角色
+     */
+    public User(int id, String username, String password, String email, String phone, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
